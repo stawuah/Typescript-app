6007f6a8d6ddd8fcf91f112b6ef168dcc0b49600
Kwasi-Awuah <stawuah18@gmail.com>
2023-06-11 11:27:03 +0000

    tsc code

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,29 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import "dotenv/config";
 import NoteModel from "./model/note";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 const app = express();
 
-app.get("/", async (req, res) => {
+app.get("/", async (_req, res, next) => {
   try {
-    throw Error("Bazinga");
+    //throw Error("Bazinga");
     const notes = await NoteModel.find().exec();
 
     res.status(200).json({ notes });
   } catch (error) {
-    console.error(error);
-    let errorMassage = " An unknown error has occured ";
-
-    if (error instanceof Error) errorMassage = error.message;
-    res.status(500).json({ error: errorMassage });
+    next(error);
   }
+});
+
+app.use((req, res, next) => {
+  next(Error("Endpoint not found"));
+});
+
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+  console.error(error);
+  let errorMassage = " An unknown error has occured ";
+  if (error instanceof Error) errorMassage = error.message;
+  res.status(404).json({ error: errorMassage });
 });
 
 export default app;
